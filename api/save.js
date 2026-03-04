abdc0a842d9546f44babc1f59a7150d02c455b73
nerdpremier <touchpeera@gmail.com>
2026-03-05 01:27:51 +0700

    d

---
+++ api/save.js
@@ -0,0 +1,36 @@
+const { Client } = require('pg');
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+
+  try {
+    const data = req.body;
+    await client.connect();
+
+    const query = `
+      INSERT INTO behavior_logs (mouse_data, click_data, key_data, idle_data, features) 
+      VALUES ($1, $2, $3, $4, $5)
+    `;
+    
+    const values = [
+      data.mouse_data || {},
+      data.click_data || {},
+      data.key_data || {},
+      data.idle_data || {},
+      data.features || {}
+    ];
+
+    await client.query(query, values);
+    await client.end();
+
+    return res.status(200).json({ message: "Success" });
+  } catch (err) {
+    if (client) await client.end();
+    return res.status(500).json({ error: err.message });
+  }
+}
